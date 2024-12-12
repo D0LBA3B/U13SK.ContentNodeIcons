fb4acbd996cb8a96444103a7a5674e273693e9bb
DOLBA3B <clementfavre755@gmail.com>
2024-12-12 19:54:29 +0100

    feat: add project to test the package

---
+++ ContentNodeIcons.Test/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Extensions;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+await app.BootUmbracoAsync();
+
+if (builder.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+app.UseStaticFiles();
+await app.RunAsync();
